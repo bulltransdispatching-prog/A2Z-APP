8e2c9d7622375a0ff82d1c817e4511c919e1ffc7
bulltransdispatching-prog <bulltransdispatching@gmail.com>
2026-02-23 14:17:56 +0500

    Add Firebase configuration to firebase.ts
    Added Firebase configuration object with necessary keys.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,1 +1,9 @@
-@import "tailwindcss";
+const firebaseConfig = {
+  apiKey: "AIzaSyCMBoABKBWTOx1B6zEBCk7QCeTKogUYgxQ",
+  authDomain: "a2z-ipm.firebaseapp.com",
+  databaseURL: "https://a2z-ipm-default-rtdb.firebaseio.com",
+  projectId: "a2z-ipm",
+  storageBucket: "a2z-ipm.firebasestorage.app",
+  messagingSenderId: "37433212653",
+  appId: "1:37433212653:web:e58af93855931236d456e4"
+};
