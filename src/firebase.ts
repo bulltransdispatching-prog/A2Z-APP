ca24e09ac6c098af343c77f6c6db650f7168f43c
bulltransdispatching-prog <bulltransdispatching@gmail.com>
2026-02-23 14:47:35 +0500

    Add Firebase Database import and initialization

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+import { getDatabase } from "firebase/database";
 const firebaseConfig = {
   apiKey: "AIzaSyCMBoABKBWTOx1B6zEBCk7QCeTKogUYgxQ",
   authDomain: "a2z-ipm.firebaseapp.com",
@@ -13,8 +9,6 @@ const firebaseConfig = {
   messagingSenderId: "37433212653",
   appId: "1:37433212653:web:e58af93855931236d456e4"
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app); 
 
