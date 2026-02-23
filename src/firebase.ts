8a01b3d39d3c20b43c2fb2694dac427b318566a4
bulltransdispatching-prog <bulltransdispatching@gmail.com>
2026-02-23 14:22:56 +0500

    Initialize Firebase with configuration

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,3 +1,9 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCMBoABKBWTOx1B6zEBCk7QCeTKogUYgxQ",
   authDomain: "a2z-ipm.firebaseapp.com",
@@ -7,3 +13,6 @@ const firebaseConfig = {
   messagingSenderId: "37433212653",
   appId: "1:37433212653:web:e58af93855931236d456e4"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
